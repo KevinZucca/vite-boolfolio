9e1dcbb4705540b35adb3a70e8d2b01bc807aac0
KevinZucca <kev.94@live.it>
2023-05-29 15:48:02 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import axios from 'axios';
+
+export default{
+  name: 'App',
+
+  data() {
+    return {
+      projects : [],
+    }
+  },
+
+  created() {
+    this.getProjects();
+  },
+
+  methods: {
+    getProjects(){
+      axios.get('http://127.0.0.1:8000/api/projects').then(response=>{
+        console.log(response);
+        this.projects = response.data.results;
+      });
+    }
+  }
+
+}
+</script>
+
+<template>
+
+  <div class="container">
+    <h1>Tutti i progetti</h1>
+
+   
+  </div>
+
+
+</template>
+
+<style lang="scss" scoped>
+
+
+</style>
