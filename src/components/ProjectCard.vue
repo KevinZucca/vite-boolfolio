83f5ada9cbc043e60a3a37c8b3b266008de06c04
KevinZucca <kev.94@live.it>
2023-05-30 17:20:17 +0200

    feat: results in page

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -5,7 +5,6 @@ export default{
 
   data() {
     return {
-        baseUrl : 'http://127.0.0.1:8000/',
     }
   },
 
@@ -43,72 +42,11 @@ export default{
 
 <template>
 
-    <div class="container">
-
-    
-        <div class="card" style="width: 18rem;">
-            <img :src="coverImage" class="card-img-top" alt="cover-img">
-            <div class="card-body">
-                <h5 class="card-title project-name">{{ project.name }}</h5>
-                <p class="card-text">{{ this.checkLength }}</p>
-                <small class="card-text">Tipologia: <strong>{{ project.type ? project.type.name : 'Nessuna Tipologia'}}</strong> </small>
-                <button class="btn btn-primary">
-                    Vai ai dettagli
-                </button>
-                <a href="#" class="card-link">{{ project.github_link }}</a>
-            </div> 
-        </div>
-
-    </div>
+  
 
 </template>
 
 <style lang="scss" scoped>
-    .card {
-        display: flex;
-        flex-flow: column;
-
-        height: 400px;
-
-        text-align: center;
-        transition: scale .2s ease-in;
-
-        &:hover {
-            scale: 1.1;
-            cursor: pointer;
-        }
-
-        .project-name {
-            text-transform: capitalize;
-        }
-
-        img {
-            width: 100%;
-            height: 40%;
-            object-fit: cover;
-        };
-
-        .card-body {
-            display: flex;
-            flex-flow: column;
-            align-items: center;
-            justify-content: center;
-      
-            a {
-                font-size: .8em;
-            }
-
-            button {
-                width: 80%;
-                transition: scale .1s ease-in;
-
-                &:hover {
-                    scale: 1.1;
-                }
-            }
-        }
-
-
-    }
+  
 
 </style>
